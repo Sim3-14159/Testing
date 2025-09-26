4bb4be8aa3e01dcd6d9dc1c0be5bdd590c65e027
Sim3-14159 <sim3.14159cuber@gmail.com>
2025-09-25 21:33:03 -0600

    Create python-test.py

---
+++ python-test.py
@@ -0,0 +1,8 @@
+# This is a python document that asks for your name and then says hi to you
+
+def main():
+    name = input("What is your name? ")
+    print("Hello,", name + "!")
+
+if __name__ == '__main__': 
+    main()
