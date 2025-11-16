1dbeed34af019bae5651a980b1d192004f471215
Sim3-14159 <sim3.14159cuber@gmail.com>
2025-11-15 20:00:50 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
-// Virtual filesystem shared with main page
 let virtualFiles = {};
 
 self.addEventListener("message", event => {
     if (event.data.type === "updateFS") {
         virtualFiles = event.data.files;
-        // console.log("FS updated", virtualFiles);
     }
 });
 
 self.addEventListener("fetch", event => {
     const url = new URL(event.request.url);
-    const path = url.pathname.slice(1); // remove leading '/'
 
-    // If the file exists in the virtual filesystem, serve it
+    // Normalize: remove GitHub repo folder
+    const path = url.pathname.split("/").pop();
+
     if (virtualFiles[path] !== undefined) {
         let type = "text/plain";
-
         if (path.endsWith(".html")) type = "text/html";
         if (path.endsWith(".css")) type = "text/css";
         if (path.endsWith(".js")) type = "application/javascript";
@@ -24,9 +22,7 @@ self.addEventListener("fetch", event => {
         if (path.endsWith(".jpg") || path.endsWith(".jpeg")) type = "image/jpeg";
 
         event.respondWith(
-            new Response(virtualFiles[path], {
-                headers: { "Content-Type": type }
-            })
+            new Response(virtualFiles[path], { headers: { "Content-Type": type } })
         );
     }
 });
